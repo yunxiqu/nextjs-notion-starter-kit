a10bf0cfd98b599cab240c81b96b385de253931f
Travis Fischer <fisch0920@gmail.com>
2022-03-29 11:30:58 -0400

    💮

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
